8472917a0337de0c00d7958e80952ca2288c46bc
Ahmedyyassen <ahmed.01014502867.y@gmail.com>
2025-08-19 16:13:34 +0300

    add path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || "/X-APP/tree/main/web",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
